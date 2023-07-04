b8335d316bce62532f4b6e86dd4b65b252afd541
Varienaja <varienaja@gmail.com>
2023-07-04 20:54:42 +0200

    Nicer display of tiles

--- perl/stationmeasurements.pl
+++ perl/stationmeasurements.pl
@@ -7,9 +7,6 @@ use CGI;
 
 my $q = CGI->new;
 my $location = $q->param('code');
-if (!$location) {
-	$location="PSI";
-}
 my $id = $q->param('id');
 if (!$id) {
 	$id = 0;
@@ -17,11 +14,15 @@ if (!$id) {
 print $q->header('application/json');
 
 my $dbh = DBI->connect("dbi:Pg:dbname=meteo;host=127.0.0.1","meteo","meteo");
-#my $query = "select array_to_json(array_agg(row_to_json(m))) from measurement m, sensor s where s.code=? and s.id=m.sensor_id and m.moment>current_date and m.id>?";
-my $query = "select array_to_json(array_agg(json_build_object('id', m.id, 'sensor_id', s.id, 'code', s.code, 'value', m.value, 'unit', s.unit, 'hh', extract(hour from m.moment), 'mm', extract(minute from m.moment) ))) from measurement m, sensor s where s.code=? and s.id=m.sensor_id and m.moment>=current_date and m.id>?";
+my $query ="select array_to_json(array_agg(json_build_object('id', m.id, 'sensor_id', s.id, 'code', s.code, 'value', m.value, 'unit', s.unit, 'hh', extract(hour from m.moment), 'mm', extract(minute from m.moment) ))) from measurement m, sensor s where s.id=m.sensor_id and m.moment>=current_date and m.id>?";
+if ($location) {
+	$query = $query." and s.code=?";
+}
 my $sth = $dbh->prepare($query);
-$sth->bind_param(1, "$location");
-$sth->bind_param(2, "$id");
+$sth->bind_param(1, "$id");
+if ($location) {
+	$sth->bind_param(2, "$location");
+}
 my $xx;
 $sth->execute();
 $sth->bind_columns(\$xx);
